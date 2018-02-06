a439a57050451fb3dd2237967d84405030ffa912
Vitor Lima <vitor.lima@msn.com>
2018-02-06 00:03:34 -0200

    Create a GrizzlyHttpServer

---
+++ src/main/java/vitor/article/validator/devmedia/resource/Server.java
@@ -0,0 +1,30 @@
+package vitor.article.validator.devmedia.resource;
+
+import java.io.IOException;
+import java.net.URI;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+/**
+ * @author Vitor Lima
+ * 
+ * Feb 5, 2018
+ *
+ */
+public class Server {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws IOException{
+		ResourceConfig config = new ResourceConfig().packages("vitor.article.validator.devmedia.resource");
+		URI uri = URI.create("http://localhost:8080");
+		HttpServer server = GrizzlyHttpServerFactory.createHttpServer(uri, config);
+		System.out.println("Server Running");
+		System.in.read();
+		server.stop();
+	}
+
+}
