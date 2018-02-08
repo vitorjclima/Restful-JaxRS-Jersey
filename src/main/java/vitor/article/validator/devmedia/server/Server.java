29af4424fe605371ed07fd2d02cc0285b858fbe9
Vitor Lima <vitor.lima@msn.com>
2018-02-08 21:42:03 -0200

    Prepare Grizzly Server for Unit Tests

--- src/main/java/vitor/article/validator/devmedia/server/Server.java
+++ src/main/java/vitor/article/validator/devmedia/server/Server.java
@@ -14,19 +14,25 @@ import org.glassfish.jersey.server.ResourceConfig;
  *
  */
 public class Server {
+	private static ResourceConfig config = new ResourceConfig().packages("vitor.article.validator.devmedia.resource");
+	private static URI uri = URI.create("http://localhost:8080");
+	private static HttpServer server = GrizzlyHttpServerFactory.createHttpServer(uri, config);
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) throws IOException{
-		ResourceConfig config = new ResourceConfig().packages("vitor.article.validator.devmedia.resource");
-		URI uri = URI.create("http://localhost:8080");
-		HttpServer server = GrizzlyHttpServerFactory.createHttpServer(uri, config);
+	public static void main(String[] args) throws IOException {
+		server.start();
 		System.out.println("Server Running");
-		if(!(args[0].equals("test"))) {
-			System.in.read();
-			server.stop();
-			System.out.println("Stopped Server");
-		}	
+		System.in.read();
+		server.stop();
+		System.out.println("Stopped Server");
+	}
+
+	/**
+	 * @return the server
+	 */
+	public static HttpServer getServer() {
+		return server;
 	}
 }
