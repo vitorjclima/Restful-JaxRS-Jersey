953ee63750fd797c52e13b80d8fb3985645d25bd
Vitor Lima <vitor.lima@msn.com>
2018-02-08 22:15:06 -0200

    Add Post Unit Test

--- src/test/java/vitor/article/validator/devmedia/resource/ResourceHelloWorldTest.java
+++ src/test/java/vitor/article/validator/devmedia/resource/ResourceHelloWorldTest.java
@@ -1,15 +1,16 @@
 package vitor.article.validator.devmedia.resource;
 
 import static com.jayway.restassured.RestAssured.get;
+import static com.jayway.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 import java.io.IOException;
 
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 
 import com.jayway.restassured.http.ContentType;
 
@@ -21,27 +22,28 @@ import vitor.article.validator.devmedia.server.Server;
  * Feb 3, 2018
  *
  */
+@TestInstance(Lifecycle.PER_CLASS)
 class ResourceHelloWorldTest {
-	
+
 	/**
 	 * 
 	 * @throws IOException 
-	 */
-	@BeforeEach
+	 */	
+	@BeforeAll
 	public void setUp() throws IOException {
-		Server.main(new String[] {"test"});
+		Server.getServer().start();
 	}
 	
-	@AfterEach
+	@AfterAll
 	public void tearDown() throws Exception {
-	   
+		Server.getServer().stop();
 	}
 
 	/**
 	 * Test method for {@link vitor.article.validator.devmedia.resource.ResourceHelloWorld#doGetAsXmlOrJson()}.
 	 */
 	@Test
-	public void testDoGetAsXmlOrJson() {
+	public void testDoGetAsText() {
 		get("/helloworld").
 		then().
 		statusCode(200).
@@ -50,5 +52,16 @@ class ResourceHelloWorldTest {
 		and().
 		body(equalTo("Get Hello World"));
 		
+	}
+	
+	@Test
+	public void testDoPutAsText() {
+		given().
+		contentType(ContentType.JSON).
+		body("{\"name\" : \"Batman\"}").
+		post("helloworld").
+		then().
+		statusCode(200).
+		and().body(equalTo("Post Hello World"));
 	}
 }
